429d792a5ca851ef9fd2a201bc5a1423e7c9f684
Daniel Cazarez <cazarez.jorge.prog2019@gmail.com>
2023-08-01 18:19:58 -0600

    Small fix

--- app/pokemon/[pokemonName]/page.tsx
+++ app/pokemon/[pokemonName]/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { getPokemon } from "../../../lib/pokeapi";
 import Image from "next/image";
-import { getStatDisplayRatio, typeStyles } from "../../../lib/utils";
+import { getStatDisplayRatio } from "../../../lib/utils";
 import { PokemonTypeStyle } from "../../../interfaces/PokemonTypeStyle";
 
 const PokedexPage = async ({
@@ -12,6 +12,117 @@ const PokedexPage = async ({
   };
 }) => {
   const pokemon = await getPokemon(params.pokemonName.toLowerCase());
+
+  const typeStyles: PokemonTypeStyle[] = [
+    {
+      name: "normal",
+      color: "bg-gray-400",
+      textColor: "text-black",
+      gradient: { from: "from-gray-400", to: "to-gray-500" },
+    },
+    {
+      name: "fire",
+      color: "bg-red-500",
+      textColor: "text-white",
+      gradient: { from: "from-red-500", to: "to-red-600" },
+    },
+    {
+      name: "water",
+      color: "bg-blue-500",
+      textColor: "text-white",
+      gradient: { from: "from-blue-500", to: "to-blue-600" },
+    },
+    {
+      name: "electric",
+      color: "bg-yellow-400",
+      textColor: "text-black",
+      gradient: { from: "from-yellow-400", to: "to-yellow-500" },
+    },
+    {
+      name: "grass",
+      color: "bg-green-500",
+      textColor: "text-white",
+      gradient: { from: "from-green-500", to: "to-green-600" },
+    },
+    {
+      name: "ice",
+      color: "bg-blue-200",
+      textColor: "text-black",
+      gradient: { from: "from-blue-200", to: "to-blue-300" },
+    },
+    {
+      name: "fighting",
+      color: "bg-red-600",
+      textColor: "text-white",
+      gradient: { from: "from-red-600", to: "to-red-700" },
+    },
+    {
+      name: "poison",
+      color: "bg-purple-500",
+      textColor: "text-white",
+      gradient: { from: "from-purple-500", to: "to-purple-600" },
+    },
+    {
+      name: "ground",
+      color: "bg-yellow-600",
+      textColor: "text-white",
+      gradient: { from: "from-yellow-600", to: "to-yellow-700" },
+    },
+    {
+      name: "flying",
+      color: "bg-blue-300",
+      textColor: "text-black",
+      gradient: { from: "from-blue-300", to: "to-blue-400" },
+    },
+    {
+      name: "psychic",
+      color: "bg-purple-400",
+      textColor: "text-black",
+      gradient: { from: "from-purple-400", to: "to-purple-500" },
+    },
+    {
+      name: "bug",
+      color: "bg-green-400",
+      textColor: "text-black",
+      gradient: { from: "from-green-400", to: "to-green-500" },
+    },
+    {
+      name: "rock",
+      color: "bg-yellow-700",
+      textColor: "text-white",
+      gradient: { from: "from-yellow-700", to: "to-yellow-800" },
+    },
+    {
+      name: "ghost",
+      color: "bg-purple-600",
+      textColor: "text-white",
+      gradient: { from: "from-purple-600", to: "to-purple-700" },
+    },
+    {
+      name: "dragon",
+      color: "bg-purple-900",
+      textColor: "text-white",
+      gradient: { from: "from-purple-900", to: "to-purple-800" },
+    },
+    {
+      name: "dark",
+      color: "bg-gray-800",
+      textColor: "text-white",
+      gradient: { from: "from-gray-800", to: "to-gray-900" },
+    },
+    {
+      name: "steel",
+      color: "bg-gray-500",
+      textColor: "text-black",
+      gradient: { from: "from-gray-500", to: "to-gray-600" },
+    },
+    {
+      name: "fairy",
+      color: "bg-pink-400",
+      textColor: "text-black",
+      gradient: { from: "from-pink-400", to: "to-pink-500" },
+    },
+  ];
 
   const pokemonTypeStyles: PokemonTypeStyle[] = [];
   typeStyles.forEach((typeStyle) => {
@@ -34,12 +145,12 @@ const PokedexPage = async ({
       {
         <div
           key={
-            pokemonTypeStyles.length === 2
+            pokemonTypeStyles.length > 1
               ? pokemonTypeStyles[1].name
               : pokemonTypeStyles[0].name
           }
           className={`${
-            pokemonTypeStyles.length === 2
+            pokemonTypeStyles.length > 1
               ? pokemonTypeStyles[1].color
               : pokemonTypeStyles[0].color
           } absolute bottom-0 left-1/2 right-0 top-0 -z-10`}
