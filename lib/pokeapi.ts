a98e84d2aceb34921168c9f332eb634d19061904
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2025-02-19 09:32:49 -0600

    Update API URL to use environment variable

--- lib/pokeapi.ts
+++ lib/pokeapi.ts
@@ -1,7 +1,7 @@
 import { Pokemon } from "../interfaces/pokemon";
 
 export const getPokemon = async (name: string) => {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/pokemon/${name}`);
   const pokemon: Pokemon = await res.json();
 
   return pokemon;
